b88f10b1a52571bca86559e2696050bca97b0220
azrael3199 <chinmayghungurde@gmail.com>
2021-09-17 17:25:31 +0530

    Created Restaurant List

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Restaurants from './assets/data.json'
+
+import RestaurantList from './components/RestaurantsList';
+
 function App() {
   return (
-    <div>
-      <h1>Eat N Treat</h1>
-    </div>
+    <Router>
+      <RestaurantList restaurants={Restaurants}/>
+    </Router>
   );
 }
 
