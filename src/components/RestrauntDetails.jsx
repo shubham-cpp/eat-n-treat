9f898d9ade63bd6544494195141ab8c54d454759
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-18 20:45:18 +0530

    Added: Button(for add/remove items)
    Still WIP because the number goes below 0

--- src/components/RestrauntDetails.jsx
+++ src/components/RestrauntDetails.jsx
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Tab, Tabs, Container } from "react-bootstrap";
 import data from "../assets/data.json";
 // import { Switch, Route, Link } from "react-router-dom";
 // props.match.params.id
 export default function RestrauntDetails(props) {
+  const id = props.match.params.id || props.id;
+  const [totalItems, setTotalItems] = useState(0);
+  const [cartList, setCartList] = useState({});
+  const [subBtnState, setSubBtnState] = useState(true);
   const restaurant = data.find(
-    ({ restaurantID }) => restaurantID === Number(props.id)
+    ({ restaurantID }) => restaurantID === Number(id)
   );
+
+  const changeCart = (obj, key) => {
+    typeof obj[key] === 'undefined' ? obj[key] = 1 : obj[key]++;
+  }
+
+  const changeCartDec = (obj, key) => {
+    typeof obj[key] === 'undefined' ? obj[key] = 0 : obj[key]--;
+  }
+
+  const addItem = (dishId) => {
+    // setCartList(prevCartList => ({
+    //   ...prevCartList,
+    //   typeof prevCartList[dishId] === 'undefined' ? prevCartList[dishId] = 1:prevCartList[dishId]++
+    // }))
+    changeCart(cartList, dishId);
+    setTotalItems(prevTotal => prevTotal + 1);
+    if (totalItems > 0)
+      setSubBtnState(false)
+  }
+
+  const removeItem = (dishId) => {
+    if (totalItems >= 0) {
+      changeCartDec(cartList, dishId)
+      // setCartList(prevCartList => (prevCartList[dishId] ?? 0) - 1)
+      setTotalItems(prevTotal => prevTotal - 1)
+      setSubBtnState(false)
+    } else {
+      setSubBtnState(true)
+    }
+  }
   console.log(restaurant);
   return (
-    /* <h2>{restaurant && restaurant.restaurantName}</h2> */
     <div className="container-fluid">
       <div className="">
         <h3 className="text-center">{restaurant.restaurantName}</h3>
@@ -29,12 +62,15 @@ export default function RestrauntDetails(props) {
                 <div className="card-title">{dish.dishName}</div>
                 <p className="card-text">
                   Rs.{dish.price} <br />
-                  {/* <span className="material-icons">Add</span> */}
-                  <span class="material-icons md-18">add_circle_outline</span>
-                  <span class="material-icons md-18">
-                    remove_circle_outline
-                  </span>
+                  <span>Item : {cartList[dish.dishID]}</span> <br />
+                  <button className="btn btn-outline-dark" onClick={() => addItem(dish.dishID)}>
+                    <span class="material-icons md-18">add_circle_outline</span>
+                  </button>
+                  <button className="btn btn-outline-dark" disabled={subBtnState} onClick={() => removeItem(dish.dishID)}>
+                    <span class="material-icons md-18"> remove_circle_outline </span>
+                  </button>
                 </p>
+                <p>Total : {totalItems}</p>
               </div>
             </div>
           );
