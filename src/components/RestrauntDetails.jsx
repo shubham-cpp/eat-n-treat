8471b5de12c61f2624f4e6af877cf98cde990dc5
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-18 19:10:45 +0530

    Added: Restraunt Details component(WIP)

--- src/components/RestrauntDetails.jsx
+++ src/components/RestrauntDetails.jsx
@@ -2,7 +2,7 @@ import React from "react";
 // import { Tab, Tabs, Container } from "react-bootstrap";
 import data from "../assets/data.json";
 // import { Switch, Route, Link } from "react-router-dom";
-
+// props.match.params.id
 export default function RestrauntDetails(props) {
   const restaurant = data.find(
     ({ restaurantID }) => restaurantID === Number(props.id)
@@ -10,8 +10,8 @@ export default function RestrauntDetails(props) {
   console.log(restaurant);
   return (
     /* <h2>{restaurant && restaurant.restaurantName}</h2> */
-    <div className="container position-relative">
-      <div className="position-absolute top-0 start-50 translate-middle">
+    <div className="container-fluid">
+      <div className="">
         <h3 className="text-center">{restaurant.restaurantName}</h3>
         <p>Location: {restaurant.location}</p>
         <p>Rating: {restaurant.rating}</p>
@@ -24,15 +24,21 @@ export default function RestrauntDetails(props) {
         <h4>Order</h4>
         {restaurant.dishes.map((dish) => {
           return (
-            <ul>
-              <li>
-                {dish.dishName} Rs.{dish.price}
-              </li>
-              {/* <p>{dish.price}</p> */}
-            </ul>
+            <div className="card">
+              <div className="card-body">
+                <div className="card-title">{dish.dishName}</div>
+                <p className="card-text">
+                  Rs.{dish.price} <br />
+                  {/* <span className="material-icons">Add</span> */}
+                  <span class="material-icons md-18">add_circle_outline</span>
+                  <span class="material-icons md-18">
+                    remove_circle_outline
+                  </span>
+                </p>
+              </div>
+            </div>
           );
         })}
-        <span className="add_circle_outline">Add</span>
       </div>
     </div>
   );
