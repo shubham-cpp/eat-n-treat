071c6da391e0a89c5f0cc69b3706fa90abffb674
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-18 19:11:03 +0530

    Commented Chinmay's code to test RestrauntDetails

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Restaurants from './assets/data.json'
-import RestaurantList from './components/RestaurantsList';
-
+import Restaurants from "./assets/data.json";
+import RestaurantList from "./components/RestaurantsList";
+import RestrauntDetails from "./components/RestrauntDetails";
 
 function App() {
   return (
-    <div id="app">
-      <Router>
-        <Switch>
-            <Route path="/" exact>
-              <RestaurantList restaurants={Restaurants}/>
-            </Route>
-            <Route path="/restaurant/:id" />
-        </Switch>
-      </Router>
-    </div>
+    // <div id="app">
+    //   <Router>
+    //     <Switch>
+    //         <Route path="/" exact>
+    //           <RestaurantList restaurants={Restaurants}/>
+    //         </Route>
+    //         <Route path="/restaurant/:id" />
+    //     </Switch>
+    //   </Router>
+    // </div>
+    <RestrauntDetails id="1001" />
   );
 }
 
