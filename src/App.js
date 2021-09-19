a9f01c8251d704bb04775c46ca3aa58d542082cc
FL45Hi <kdshinde975@yahoo.com>
2021-09-19 10:53:13 +0530

    login and signup components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Restaurants from "./assets/data.json";
 import RestaurantList from "./components/RestaurantsList";
 import RestrauntDetails from "./components/RestrauntDetails";
+import Landing from "./components/Landing";
+
+import { dividerClasses } from "@mui/material";
 
 function App() {
   return (
@@ -16,7 +19,9 @@ function App() {
             <Route path="/restaurant/:id" component={RestrauntDetails} />
         </Switch>
       </Router>
-    </div>
+      
+    </div>    
+
   );
 }
 
