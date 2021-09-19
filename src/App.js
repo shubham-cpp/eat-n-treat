90048d6e029f762a1c6bbba2e2de4a5734dae062
oj1001 <omkarjadhav880@gmail.com>
2021-09-19 12:08:20 +0530

    conflcits

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Restaurants from "./assets/data.json";
+import RestaurantList from "./components/RestaurantsList";
+import RestrauntDetails from "./components/RestrauntDetails";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Searchbar from "./components/Searchbar";
@@ -24,41 +10,9 @@ import Chatbot from "./components/Chatbotcomp";
 function App() {
   return (
     <div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
      <Navbar/>
      <Searchbar/>
      <Chatbot/>
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import Restaurants from "./assets/data.json";
-import RestaurantList from "./components/RestaurantsList";
-import RestrauntDetails from "./components/RestrauntDetails";
-
-function App() {
-  return (
-    <div id="app">
       <Router>
         <Switch>
             <Route path="/" exact>
@@ -67,7 +21,6 @@ function App() {
             <Route path="/restaurant/:id" component={RestrauntDetails} />
         </Switch>
       </Router>
->>>>>>> 9f898d9ade63bd6544494195141ab8c54d454759
     </div>
   );
 }
