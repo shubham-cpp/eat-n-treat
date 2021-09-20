4959fb726944240715894ab42851590f1d657045
urmilasalke <urmilasalke@gmail.com>
2021-09-20 13:24:19 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const dotEnv = require("dotenv")
+console.log("process.env.DATABASE_URL")
+console.log("process.env.PORT")
+dotEnv.config()
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+mongoose.connect(process.env.DATABASE_URL,
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }
+    )
+    .then(() => console.log("Connected to db"))
+    .catch((err) => { console.log("err") 
+})
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from Express!" });
-  });
-  
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
-
-app.get('/', function(req, res, next) {
-    res.send("Express server running");
-});
+app.use("/customer",require("./routes/customer"));
+app.use("/restaurant",require("./routes/resturants"));
+app.listen(process.env.PORT, () => { console.log("Server is running") })
--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ function App() {
   return (
     <div>
       <h1>Eat N Treat</h1>
-      <h2>Hello</h2>
     </div>
   );
 }
