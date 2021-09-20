e6c45a184de25b5348df6f12f3aae1ce16ffc75d
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-20 20:05:56 +0530

    Better implementation of Details and Cart
    I have created a seperate folder for Restraunt Details
    
    Main: This file includes functions like addToCart and removeFromCart
    Cart: Will handle items that Customer has added to cart
    DIsh: Will render individual dish passed as props
    
    Also deleted old RestrauntDetails file since we follow new approarch

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Restaurants from "./assets/data.json";
 import RestaurantList from "./components/RestaurantsList";
-import RestrauntDetails from "./components/RestrauntDetails";
+import { Main as RestDetails } from "./components/RestrauntDetails/Main";
 
 function App() {
   return (
     <div id="app">
       <Router>
         <Switch>
-            <Route path="/" exact>
-              <RestaurantList restaurants={Restaurants}/>
-            </Route>
-            <Route path="/restaurant/:id" component={RestrauntDetails} />
+          <Route path="/" exact>
+            <RestaurantList restaurants={Restaurants} />
+          </Route>
+          <Route path="/restaurant/:id" component={RestDetails} />
         </Switch>
       </Router>
     </div>
