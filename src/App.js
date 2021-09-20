21fd996cfa9ec1a2811d52731a1a8322be68cc58
urmilasalke <urmilasalke@gmail.com>
2021-09-20 13:36:10 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Restaurants from "./assets/data.json";
+import RestaurauntList from './components/RestaurantList'
+import RestrauntDetails from "./components/RestrauntDetails";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Searchbar from "./components/Searchbar";
+import Chatbot from "./components/Chatbotcomp";
 function App() {
   return (
     <div>
-      <h1>Eat N Treat</h1>
+     <Navbar/>
+     <Searchbar/>
+     <Chatbot/>
+     <Router>
+        <Switch>
+            <Route path="/" exact>
+              <RestaurauntList restaurants={Restaurants}/>
+            </Route>
+            <Route path="/restaurant/:id" component={RestrauntDetails} />
+        </Switch>
+      </Router>
+     
+      
     </div>
   );
 }
-
 export default App;
