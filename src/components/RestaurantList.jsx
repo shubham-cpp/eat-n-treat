6b389040bc7734f5162487a77d3071680658cdb9
oj1001 <omkarjadhav880@gmail.com>
2021-09-21 10:19:53 +0530

    modified restaraunt list to implement search functionality

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,16 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Restaurants from  '../assets/data.json'
 import RestaurantCard from "./RestaurantCard";
-
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 export default function RestaurantList(props) {
+    const [res, setRes] = useState([])
+
+    const filterdata = Restaurants.filter(item => {
+        return res !== "" ? item.restaurantName.toLowerCase().includes(res) || item.location.toLowerCase().includes(res) : item
+    })
     return (
-        <div className="container">
+        <div className="container" style={{marginTop:"0",verticalAlign:"auto"}}>
+            <div className="search">
+                <div className="searchInputs">
+                    <input
+                        type="text"
+                        placeholder="Enter Restaraunt Name or location"
+                        onChange={(event) => {
+                            setRes(event.target.value);
+                        }}
+                    />
+                    <div className="searchIcon">
+                        <SearchOutlinedIcon />
+                    </div>
+                </div>
+            </div>
             <div className="row flex-row">
                 {
-                    props.restaurants.map(restaurant => {
+                    filterdata.map(restaurant => {
                         return (
                             <div className="col-3 pt-4">
-                                <RestaurantCard restaurant={restaurant}/>
+                                <RestaurantCard restaurant={restaurant} />
                             </div>
                         );
                     })
