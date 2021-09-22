ff53edf07781b24d603e96fd97982e4a50835f36
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-22 12:31:39 +0530

    Removed Unnecessary css file

--- src/components/Chatbotcomp.jsx
+++ src/components/Chatbotcomp.jsx
@@ -1,36 +1,37 @@
-import React, { useState } from 'react'
-import { Chatbot } from 'react-chatbot-kit'
-import ActionProvider from './actionprovider'
-import config from './config'
-import MessageParser from './messageparser'
-import './chatbotcomp.css'
+import React, { useState } from "react";
+import { Chatbot } from "react-chatbot-kit";
+import ActionProvider from "./actionprovider";
+import config from "./config";
+import MessageParser from "./messageparser";
 
-import ChatBubble from '@material-ui/icons/ChatBubble';
-
+import ChatBubble from "@material-ui/icons/ChatBubble";
 
 function Chatbotcomp() {
   const [showBot, toggleBot] = useState(false);
 
   return (
     <div>
-      <div style={{marginLeft:"850px",position:"fixed",zIndex:11}}>
-      {showBot && (
-        <Chatbot
-          config={config}
-          actionProvider={ActionProvider}
-          messageParser={MessageParser}
-        />
-      )}
+      <div style={{ marginLeft: "850px", position: "fixed", zIndex: 11 }}>
+        {showBot && (
+          <Chatbot
+            config={config}
+            actionProvider={ActionProvider}
+            messageParser={MessageParser}
+          />
+        )}
       </div>
-      <div  style={{position:"fixed",zIndex:10,float:"right"}}>
-        <button className="open-button" onClick={() => toggleBot((prev) => !prev)}>
-          <ChatBubble style={{ height: "30px", width: "30px" }}/>
+      <div style={{ position: "fixed", zIndex: 10, float: "right" }}>
+        <button
+          className="open-button"
+          onClick={() => toggleBot((prev) => !prev)}
+        >
+          <ChatBubble style={{ height: "30px", width: "30px" }} />
         </button>
         <span></span>
         <span></span>
       </div>
     </div>
-  )
+  );
 }
 
-export default Chatbotcomp
+export default Chatbotcomp;
