921f6c5cd69049bd103226137aba6e9d01e2f2c4
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-28 07:19:30 +0530

    Made dished reusable
    Now we can pass props like handler function and button name

--- src/components/RestrauntDetails/Dish.jsx
+++ src/components/RestrauntDetails/Dish.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-export default function Dish({ dish, addToCart }) {
+export default function Dish({ dish, handleFunction, btnName }) {
   return (
     <div className="card mx-2" style={{ width: "20rem" }}>
       <div className="card-title">{dish.menuName}</div>
       <p className="card-text">
         Rs.{dish.menuPrice} <br />
         <button
-          onClick={() => addToCart(dish)}
+          onClick={handleFunction}
           className="btn btn-outline-success float-end m-2"
         >
-          Add
+          {btnName}
         </button>
       </p>
     </div>
