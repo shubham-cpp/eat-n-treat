d965b582eda67299928ef8d3322f2011234e5a49
oj1001 <omkarjadhav880@gmail.com>
2021-09-28 16:51:16 +0530

    changed models and route modified navbar

--- src/auth.js
+++ src/auth.js
@@ -10,7 +10,6 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
-  console.log(currentUser);
 
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
