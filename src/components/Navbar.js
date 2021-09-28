7411765264c75577d5418ee8ba6970d8c7aba0e6
azrael3199 <chinmayghungurde@gmail.com>
2021-09-28 22:49:27 +0530

    Added option to change Navbar content for admin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,33 +7,44 @@ import { Link } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 
-function NavBar() {
+function NavBar(props) {
   return (
-    <div className="entry">
-
-      <Navbar className="color-bg fixed-top" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/" style={{ textDecoration: "none" }}>
-              Eat and Treat
-            </Link>
-          </Navbar.Brand>
-          <Nav className="items">
-            <Nav.Link>
-              <Link to="/" style={{ textDecoration: "none" }}>
-                {/* Login */}
-                <Login />
-              </Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/" style={{ textDecoration: "none" }}>
-                {/* Sign up */}
-                <Signup />
-              </Link>
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <div>
+      <div className="entry">
+        <Navbar className="color-bg fixed-top" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand>
+              {!props.change ? (
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  Eat and Treat
+                </Link>
+              ) : (
+                <Link to="/admin" style={{ textDecoration: "none" }}>
+                  Admin Panel
+                </Link>
+              )}
+            </Navbar.Brand>
+            {!props.change ? (
+              <Nav className="items">
+                <Nav.Link>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    {/* Login */}
+                    <Login />
+                  </Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/" style={{ textDecoration: "none" }}>
+                    {/* Sign up */}
+                    <Signup />
+                  </Link>
+                </Nav.Link>
+              </Nav>
+            ) : (
+              <></>
+            )}
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 }
