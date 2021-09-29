e2b33966f65a6da0c85c7ecec8e7b75799ff2700
shubham_cpp <shubhampawar3007@gmail.com>
2021-09-29 12:25:24 +0530

    show total amount spent on website

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -13,6 +13,10 @@ export default function Orders() {
       .catch((err) => console.log("Error in axios request for order ", err));
   }, []);
 
+  const sumTotal = orders.reduce(
+    (total, order) => total + order.totalAmount,
+    0
+  );
   return (
     <div className="container" style={{ marginTop: "5rem" }}>
       <h1>Welcome to orders page</h1>
@@ -30,16 +34,19 @@ export default function Orders() {
             <tbody>
               {order.cart.map((item, index) => (
                 <tr>
-                  <th scope="row">{index}</th>
+                  <th scope="row">{index + 1}</th>
                   <td>{item.menuName}</td>
                   <td>{item.menuPrice}</td>
                 </tr>
               ))}
             </tbody>
           </table>
-          <h5>Total : {order.totalAmount}</h5>
+          <h6>Status: {order.orderStatus}</h6>
+          <h6>Total : {order.totalAmount}</h6>
         </div>
       ))}
+
+      <h3>Total Amount Spent: {sumTotal}</h3>
     </div>
   );
 }
