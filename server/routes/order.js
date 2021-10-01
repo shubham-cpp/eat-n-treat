1e2581738a2206eb3885ba88aae34c7d0350326c
shubham_cpp <shubhampawar3007@gmail.com>
2021-10-01 15:05:43 +0530

    reformatted order.js file

--- server/routes/order.js
+++ server/routes/order.js
@@ -5,7 +5,9 @@ const { body, validationResult } = require("express-validator");
 const Orders = require("../model/order");
 
 router.get("/", (_, res) => {
-  Orders.find({}).then((data) => res.json(data)).catch(err => console.log(err));
+  Orders.find({})
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
 });
 
 router.post(
@@ -51,17 +53,17 @@ router.patch("/:orderid", (req, res) => {
     { $set: { orderStatus: "Delivered" } },
     { new: false, upsert: false }
   )
-    .then((data) => res.json({ "Order Status Updated": data }))
+    .then((data) => res.json(data))
     .catch((err) => res.json({ "Failed to update order status": err.message }));
 });
 
 router.get("/:rid", (req, res) => {
-  const restrauntID = req.params.rid;
+  const restaurauntID = req.params.rid;
 
-  Orders.find({ restrauntID: restrauntID })
+  Orders.find({ restrauntID: restaurauntID })
     .then((allOrders) => res.json(allOrders))
     .catch((err) =>
-      res.json("Caught while fetching restraunt orders:", err.message)
+      res.json({ "Caught while fetching restraunt orders": err.message })
     );
 });
 
@@ -70,7 +72,7 @@ router.get("/customer/:customerid", (req, res) => {
   Orders.find({ customerID: customerID })
     .then((customerDetails) => res.json(customerDetails))
     .catch((err) =>
-      res.json("Caught while fetch customer orders:", err.message)
+      res.json({ "Caught while fetch customer orders": err.message })
     );
 });
 
