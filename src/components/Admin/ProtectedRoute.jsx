bd919d005036cbf9e7e91c3fa8fadb44fe8da7a7
azrael3199 <chinmayghungurde@gmail.com>
2021-10-03 12:19:52 +0530

    Customized protected route

--- src/components/Admin/ProtectedRoute.jsx
+++ src/components/Admin/ProtectedRoute.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom';
+import React from "react";
+import { Route, Redirect, useParams } from "react-router-dom";
 
-const ProtectedRoute = ({ auth, component: Component, ...rest }) => {
-    return <Route 
-        {...rest}
-        render={props =>
-            auth ? (
-                <Component {...rest} {...props} />
-            ): (
-                <Redirect 
-                    to={{
-                        pathname: "/admin"
-                    }}
-                />
-            )
-        }
+const ProtectedRoute = ({
+  auth,
+  pathname,
+  exact,
+  matchID,
+  component: Component,
+  ...rest
+}) => {
+  const { id } = useParams();
+  console.log(id + "   " + matchID);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        auth && id === matchID ? (
+          <Component {...rest} {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: pathname,
+            }}
+          />
+        )
+      }
+      exact={exact}
     />
-}
+  );
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
